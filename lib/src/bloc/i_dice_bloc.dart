4b5836d2380ac95b2e14c4ff45fc35acf127d25a
kevin-guillen <kevin.guillen@globant.com>
2021-06-17 16:41:20 -0300

    Functionalities implementation with BLoC pattern

--- lib/src/bloc/i_dice_bloc.dart
+++ lib/src/bloc/i_dice_bloc.dart
@@ -1,24 +1,10 @@
-import 'package:dice_roller/src/bloc/dice_bloc.dart';
+import 'package:flutter/cupertino.dart';
 
-class DiceRoll extends IDiceRollBloc{
-  @override
-  // TODO: implement currentState
-  int get currentState => throw _DicePageState();
+import 'bloc.dart';
 
-  @override
-  void diceChanger() {
-    // TODO: implement diceChanger
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-  }
-
-  @override
-  Future<void> initialize() {
-    // TODO: implement initialize
-    throw UnimplementedError();
-  }
-
+abstract class IDiceBloc extends Bloc {
+  Stream<List<AssetImage>> get valuesStream;
+  List<AssetImage> defaultDice();
+  bool get dicesAreEqual;
+  void diceChanger();
 }
